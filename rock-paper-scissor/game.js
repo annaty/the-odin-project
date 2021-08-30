18d053ee3176df2baa80e2e3afa3e68479e9026b
annaty <tylkowskanna@gmail.com>
2021-08-30 18:26:40 +0200

    fixed the broken callback, buttons work

--- rock-paper-scissor/game.js
+++ rock-paper-scissor/game.js
@@ -1,55 +1,58 @@
 const rock = "rock";
 const paper = "paper";
 const scissor = "scissor";
-
 let playerPoints = 0;
 let computerPoints = 0;
 let message = "";
 
 function computerPlay() {
-    let randomNumber = Math.floor(Math.random() * 3)
+    let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-            return "rock";
+            return rock;
         case 1:
-            return "paper";
+            return paper;
         case 2:
-            return "scissor";
-    }
+            return scissor;
+    } 
 }
-
-const event_handler1 = (playRound, rock) => playRound(rock);
-
-document.getElementById("button-rock").addEventListener("click", event_handler1); 
-document.getElementById("button-paper").addEventListener("click", playRound("paper")); 
-document.getElementById("button-scissor").addEventListener("click", playRound("scissor"));
 
 function playRound(playerSelection) {
-    let computerSelection = computerPlay;
-
-    if (playerSelection === "rock" && computerSelection === "scissor") {
+    let computerSelection = computerPlay();
+    if (playerSelection === rock && computerSelection === scissor) {
         playerPoints++;
-        message = "The player wins! Rock beats scissor"
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        message = "The player wins! Rock beats scissor";
+    } else if (playerSelection === rock && computerSelection === paper) {
         computerPoints++;
-        message = "The computer wins! Paper beats rock"
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+        message = "The computer wins! Paper beats rock";
+    } else if (playerSelection === paper && computerSelection === scissor) {
         computerPoints++;
-        message = "The computer wins! Scissor beats paper"
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        message = "The computer wins! Scissor beats paper";
+    } else if (playerSelection === paper && computerSelection === rock) {
         playerPoints++;
-        message = "The player wins! Paper beats rock"
-    } else if (playerSelection === "scissor" && computerSelection === "paper") {
+        message = "The player wins! Paper beats rock";
+    } else if (playerSelection === scissor && computerSelection === paper) {
         playerPoints++;
-        message = "The player wins! Scissor beats paper"
-    } else if (playerSelection === "scissor" && computerSelection === "rock") {
+        message = "The player wins! Scissor beats paper";
+    } else if (playerSelection === scissor && computerSelection === rock) {
         computerPoints++;
-        message = "The computer wins! Rock beats scissor"
+        message = "The computer wins! Rock beats scissor";
     } else if (playerSelection === computerSelection) {
-        message = "It's a tie!"
+        message = "It's a tie!";
     } else {
-        message = "please input a correct value"
+        message = "please input a correct value";
     }
-    
     document.getElementById("statusMessage").innerHTML = message;
 }
+
+
+document.getElementById("button-rock").addEventListener("click", () => {
+    playRound(rock);
+}); 
+document.getElementById("button-paper").addEventListener("click", () => {
+    playRound(paper);
+}); 
+document.getElementById("button-scissor").addEventListener("click", () => {
+    playRound(scissor);
+});
+
